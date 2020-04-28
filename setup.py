86df4fa394f7411ba8e661cc9ef3ad1403eb4402
Kelly <guojiyou40020@hotmail.com>
2020-04-28 15:51:09 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-        name='Eww',
+        name='Conger',
         version='0.1.0',
         packages=['conger'],
-        url='',
+        url='https://github.com/guo40020/Conger',
         license='MIT',
         author='Kelly',
         author_email='',
